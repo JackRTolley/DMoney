996a5a5eca291347225199fb1cb9695acf30c650
Jack Tolley <JackRTolley@gmail.com>
2019-11-23 21:57:53 +0000

    added hello_world

--- hello_world.py
+++ hello_world.py
@@ -10,5 +10,20 @@ def hello_world():
    ]
    return render_template('index.html', title='Home', projects=projects)
 
+@app.route('/account')
+def account():
+   transactions = [
+      {'name': 'Help Cats', 'invested': 50, 'funded': 0.8, 'return': 0},
+      {'name': 'Help Dogs', 'invested': 20, 'funded': 1, 'return': 2}
+   ]
+   data = {
+      'investments': 100,
+      'donations': 200,
+      'returns': 10
+   }
+   return render_template('account.html', title='My Account', transactions=transactions, data=data)
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
