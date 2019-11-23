9e03f8275b8969b1003ae9c63c6c4cc63846ef8e
Jack Tolley <JackRTolley@gmail.com>
2019-11-23 22:43:56 +0000

    modified models

--- app/models.py
+++ app/models.py
@@ -5,11 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 DB_URL = 'sqlite:///database.db'
-
-
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,17 +14,17 @@ app.config['SECRET_KEY'] = 'secret-key'
 db = SQLAlchemy(app)
 
 # Create our database model
-<<<<<<< HEAD:projects.py
-=======
+
 class Project(db.Model):
     __tablename__ = "projects"
     id = db.Column(db.Integer, primary_key=True)
     creator_id = db.Column(db.Integer, unique=True)
+    #creator_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable = False)
     title = db.Column(db.String(120), unique=True)
     description = db.Column(db.String(1000), unique = True)
     score = db.Column(db.Integer)
     total_funding = db.Column(db.Float, unique=False)
->>>>>>> 03668d3858d857a2eefac88b8d38ac1c80327b57:app/models.py
+    #transactions = db.relationship('Transaction',backref='projects',lazy=True)
 
 class User(db.Model):
     __tablename__="users"
@@ -40,6 +36,7 @@ class User(db.Model):
     credit = db.Column(db.Float, unique=False)
     projects = db.relationship('Project',backref='users',lazy=True)
     transactions = db.relationship('Transaction',backref='users',lazy=True)
+
 class Project(db.Model):
     __tablename__ = "projects"
     id = db.Column(db.Integer, primary_key=True)
@@ -48,6 +45,7 @@ class Project(db.Model):
     total_funding = db.Column(db.Float, unique=False)
     creator_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable = False)
     transactions = db.relationship('Transaction',backref='projects',lazy=True)
+
 class Transaction(db.Model):
     __tablename__="transactions"
     id = db.Column(db.Integer, primary_key=True)
@@ -62,7 +60,7 @@ def count():
 def colunm_names():
     print()
 
-app.cli.command('resetdb')
+@app.cli.command('resetdb')
 def resetdb_command():
     """Destroys and creates the database + tables."""
 
@@ -78,18 +76,7 @@ def resetdb_command():
     db.create_all()
     print('Shiny!')
 
-resetdb_command()
 
-
-
-project1 = Project(id = 1, title="Building a Road", score = 3, total_funding=100)
-project2 = Project(id = 2, title="Local Mentoring", score = 10, total_funding=100)
-project3 = Project(id = 3, title="Mental Health Care", score = 5, total_funding=100)
-project4 = Project(id = 4, title="Durham Litter Picking", score=0, total_funding=100)
-project5 = Project(id = 5, title="Park Management", score=6, total_funding=100)
-
-
-user1 = User(id =1, name="jwpetley", display_name="James Petley", email="jwpetley@gmail.com", location="Durham", credit=100)
 
 @app.cli.command('add_sample_data')
 def add_data():
@@ -107,7 +94,7 @@ def add_data():
     session.add(project5)
     session.commit()
 
-<<<<<<< HEAD:projects.py
+
 def get_projects_by_score():
     engine = create_engine(DB_URL, echo=True)
     Session = sessionmaker(bind=engine)
@@ -122,20 +109,29 @@ def get_projects_by_score():
         dict = {'id':id, 'name':name, 'score':score, creator:'creator'}
         list.append(dict)
     print(list)
-=======
 
-
->>>>>>> 03668d3858d857a2eefac88b8d38ac1c80327b57:app/models.py
 
 if __name__ == "__main__":
     engine = create_engine(DB_URL, echo=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-<<<<<<< HEAD:projects.py
     user_list = session.query(user).first()
     print(user_list.name)
-=======
+
     user_list = session.query(User).first()
->>>>>>> 03668d3858d857a2eefac88b8d38ac1c80327b57:app/models.py
+
 
     get_projects_by_score()
+
+    resetdb_command()
+
+
+
+project1 = Project(id = 1, title="Building a Road", score = 3, total_funding=100)
+project2 = Project(id = 2, title="Local Mentoring", score = 10, total_funding=100)
+project3 = Project(id = 3, title="Mental Health Care", score = 5, total_funding=100)
+project4 = Project(id = 4, title="Durham Litter Picking", score=0, total_funding=100)
+project5 = Project(id = 5, title="Park Management", score=6, total_funding=100)
+
+
+user1 = User(id =1, name="jwpetley", display_name="James Petley", email="jwpetley@gmail.com", location="Durham", credit=100)
