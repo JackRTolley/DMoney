157fc9e2f43c117f281efb4e91ba66cd453bccf9
James Petley <jwpetley@gmail.com>
2019-11-23 16:38:50 +0000

    Database can be Queried

--- projects.py
+++ projects.py
@@ -2,24 +2,34 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_URL = 'sqlite:///database.db'
 
 
-conn = sqlite3.connect('database.db')
 
-DB_URL = 'sqlite:///database.sqlite3'
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 db = SQLAlchemy(app)
 
 # Create our database model
-class Projects(db.Model):
+class Project(db.Model):
     __tablename__ = "projects"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True)
     score = db.Column(db.Integer)
 
 
+
+
+def count():
+    print("Total number of projects is", Project.query.count())
+
+def colum_names():
+    print()
 
 
 
@@ -38,3 +48,27 @@ def resetdb_command():
     print('Creating tables.')
     db.create_all()
     print('Shiny!')
+
+
+
+engine = create_engine(DB_URL, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+project1 = Project(id = 1, title="Building a Road", score = 3)
+project2 = Project(id = 2, title="Local Mentoring", score = 10)
+project3 = Project(id = 3, title="Mental Health Care", score = 5)
+project4 = Project(id = 4, title="Durham Litter Picking", score=0)
+project5 = Project(id = 5, title="Park Management", score=6)
+
+ordered_list = session.query(Project).filter_by(Project.score).all()
+
+print(ordered_list)
+
+'''session.add(project1)
+session.add(project2)
+session.add(project3)
+session.add(project4)
+session.add(project5)
+session.commit()'''
