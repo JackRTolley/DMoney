e8c7637fc5407f255252b82b767c6116904952a0
James Petley <jwpetley@gmail.com>
2019-11-23 18:53:55 +0000

    Added new properties to classes and an add_sample_data command

--- projects.py
+++ projects.py
@@ -22,16 +22,24 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True)
     score = db.Column(db.Integer)
-
+    total_funding = db.Column(db.Float, unique=False)
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__="users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False,nullable=False)
     display_name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     location = db.Column(db.String(255),unique=False, nullable=False)
+    credit = db.Column(db.Float, unique=False)
 
+class Transaction(db.Model):
+    __tablename__="transactions"
+    id = db.Column(db.Integer, primary_key=True)
+    user_key = db.Column(db.Integer, unique=True)
+    project_key = db.Column(db.Integer, unique=True)
+    value = db.Column(db.Float, unique=False)
+    #rate = db.Column(db.Float)
 
 
 
@@ -60,33 +68,42 @@ def resetdb_command():
     db.create_all()
     print('Shiny!')
 
+#resetdb_command()
 
 
-engine = create_engine(DB_URL, echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
 
 
-project1 = Project(id = 1, title="Building a Road", score = 3)
-project2 = Project(id = 2, title="Local Mentoring", score = 10)
-project3 = Project(id = 3, title="Mental Health Care", score = 5)
-project4 = Project(id = 4, title="Durham Litter Picking", score=0)
-project5 = Project(id = 5, title="Park Management", score=6)
+project1 = Project(id = 1, title="Building a Road", score = 3, total_funding=100)
+project2 = Project(id = 2, title="Local Mentoring", score = 10, total_funding=100)
+project3 = Project(id = 3, title="Mental Health Care", score = 5, total_funding=100)
+project4 = Project(id = 4, title="Durham Litter Picking", score=0, total_funding=100)
+project5 = Project(id = 5, title="Park Management", score=6, total_funding=100)
 
 
-user1 = User(id =1, name="jwpetley", display_name="James Petley", email="jwpetley@gmail.com", location="Durham")
-#session.add(user1)
-#session.commit()
-user_list = session.query(User).first()
-print(user_list.email)
+user1 = User(id =1, name="jwpetley", display_name="James Petley", email="jwpetley@gmail.com", location="Durham", credit=100)
 
-ordered_list = session.query(Project).order_by(Project.score)
-print(ordered_list.all())
+@app.cli.command('add_sample_data')
+def add_data():
+    engine = create_engine(DB_URL, echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(user1)
 
 
-'''session.add(project1)
-session.add(project2)
-session.add(project3)
-session.add(project4)
-session.add(project5)
-session.commit()'''
+    session.add(project1)
+    session.add(project2)
+    session.add(project3)
+    session.add(project4)
+    session.add(project5)
+    session.commit()
+
+if __name__ == "__main__":
+    engine = create_engine(DB_URL, echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user_list = session.query(User).first()
+    print(user_list.email)
+
+    ordered_list = session.query(Project).order_by(Project.score)
+    print(ordered_list.all())
