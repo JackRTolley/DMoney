f9aff9788ff07c7d6d7eca8b7d1b885805d3b577
James Petley <jwpetley@gmail.com>
2019-11-23 15:22:46 +0000

    New requirements

--- projects.py
+++ projects.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+POSTGRES_URL="127.0.0.1:5432"
+POSTGRES_USER="dmoney"
+POSTGRES_PW="password"
+POSTGRES_DB="test"
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,
+        pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+#app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Create our database model
@@ -10,15 +20,24 @@ class Projects(db.Model):
     __tablename__ = "projects"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True)
-    score = db.Column(db.Int())
+    score = db.Column(db.Integer)
 
-    def __init__(self, email):
-        self.email = email
 
-    def __repr__(self):
-        return '<E-mail %r>' % self.email
 
-# Set "homepage" to index.html
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+
+@app.cli.command('resetdb')
+def resetdb_command():
+    """Destroys and creates the database + tables."""
+
+    from sqlalchemy_utils import database_exists, create_database, drop_database
+    if database_exists(DB_URL):
+        print('Deleting database.')
+        drop_database(DB_URL)
+    if not database_exists(DB_URL):
+        print('Creating database.')
+        create_database(DB_URL)
+
+    print('Creating tables.')
+    db.create_all()
+    print('Shiny!')
